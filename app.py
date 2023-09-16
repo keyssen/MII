f3a0f14af858a072788e5c2056caae2f7068c3c1
Николай <raspaev.niko@mail.ru>
2023-09-16 15:59:52 +0400

    Corrected lab

--- app.py
+++ app.py
@@ -1,43 +1,27 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import numpy as np
+
 app = Flask(__name__)
+df = pd.read_csv('TSLA.csv')
 
 def get_column_description(df):
-    print(df.describe())
-    result = pd.DataFrame({'types': df.dtypes,'empty': df.isnull().sum(), 'fill': df.notnull().sum()})
-    return result
+    result = pd.DataFrame({'columns': df.columns, 'types': df.dtypes,'empty': df.isnull().sum(), 'fill': df.notnull().sum()})
+    list = result.values.tolist()
+    list.sort(key = lambda x:x[0])
+    return list
+
 @app.route('/', methods=['GET', 'POST'])
 def table():
-    params = {
-        "data": table,
-        "start_index": 0
-    }
-
-    df = pd.read_csv('TSLA.csv')
-    # length = df.shape
-    # print(df.shape[0]) # строк
-    # print(df.shape[1])  # столбцов
-
-
     column_descriptions = get_column_description(df)
-
-    types = df.dtypes
-    df.describe()
     if request.method == "POST":
         minCol = int(request.form["minCol"])
         maxCol = int(request.form["maxCol"])
         minRow = int(request.form["minRow"])
         maxRow = int(request.form["maxRow"])
-        return render_template('index.html', tables=[df.iloc[minRow:maxRow+1, minCol-1:maxCol].to_html(classes="container table"),
-        column_descriptions.to_html(classes="container table")])
+        return render_template('index.html', tables=[
+            df.iloc[minRow:maxRow + 1, minCol - 1:maxCol].to_html(classes="container table table-bordered mystyle")],column_descriptions=column_descriptions)
     else:
-        return render_template('index.html', tables=[df.to_html(classes="container table"),
-        column_descriptions.to_html(classes="container table")])
-
-@app.route('/about')
-def about():
-    return "about"
+        return render_template('index.html', tables=[df.iloc[0:15, 0:9].to_html(classes="container table table-bordered mystyle")], column_descriptions=column_descriptions)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=int("5000"))
